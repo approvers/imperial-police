fef02efb302734e6b10dbef1693a34b6e34641e6
Colk-tech <colk.tech@gmail.com>
2021-03-23 06:42:39 +0900

    [追加] あーだこーだした
    (cherry picked from commit 81d7bafeb203fe3566791a18a35188375e7c3efc)
    
    # Conflicts:
    #	requirements.txt
    #	src/client/main_client.py
    #	src/handler/me_handler.py
    #	src/handler/voice.py
    #	src/service/embed/exception.py
    #	src/service/voice/play_sound.py

--- src/service/voice/play_sound.py
+++ src/service/voice/play_sound.py
@@ -4,10 +4,9 @@ from typing import Optional
 
 import discord
 
-from config import NATIONAL_ANTHEM, VC_STAY_LENGTH, PRISON_CHANNEL_ID
+from config import NATIONAL_ANTHEM, VC_STAY_LENGTH, PRISON_CHANNEL_ID, ROYAL_ROOM_ID
 from src.service.voice.voice_abs import VoiceFunctionAbstract
 from src.service.misc.royal_judge import RoyalJudge
-from src.exception.misunderstanding import MisunderstandingException
 from src.client.global_client import GlobalClient
 
 
@@ -16,16 +15,8 @@ class PlaySound(VoiceFunctionAbstract, ABC):
     PRISON_CHANNEL: Optional[discord.VoiceChannel] = None
 
     @classmethod
-    async def disconnected(cls):
+    def disconnected(cls):
         cls.IS_EXECUTING = False
-
-        number_of_voice_clients: int = len(GlobalClient.client.voice_clients)
-        if number_of_voice_clients > 1:
-            raise MisunderstandingException("The coder thought there can be only one or no voice client,"
-                                            "but got {}.".format(number_of_voice_clients))
-
-        if number_of_voice_clients == 1:
-            await GlobalClient.client.voice_clients[0].disconnect()
 
     def __init__(self, member: discord.Member, after: discord.VoiceState, is_join: bool):
         PlaySound.static_check()
@@ -43,9 +34,6 @@ class PlaySound(VoiceFunctionAbstract, ABC):
     async def is_triggered(self) -> bool:
         self._is_triggered = False
 
-        if PlaySound.IS_EXECUTING:
-            return self._is_triggered
-
         if not self.is_join and self.is_join is not None:
             return self._is_triggered
 
@@ -55,10 +43,8 @@ class PlaySound(VoiceFunctionAbstract, ABC):
         if RoyalJudge.is_royal_family_member_from_id(self.member.id):
             return self._is_triggered
 
-        number_of_voice_clients: int = len(GlobalClient.client.voice_clients)
-        if number_of_voice_clients > 1:
-            raise MisunderstandingException("The coder thought there can be only one or no voice client,"
-                                            "but got {}.".format(number_of_voice_clients))
+        if PlaySound.IS_EXECUTING:
+            return self._is_triggered
 
         self._is_triggered = True
         return self._is_triggered
@@ -67,25 +53,14 @@ class PlaySound(VoiceFunctionAbstract, ABC):
         if not self._is_triggered:
             return
 
-        voice_client: discord.VoiceClient = await PlaySound.PRISON_CHANNEL.connect(reconnect=False)
-        voice_client.play(discord.FFmpegPCMAudio(NATIONAL_ANTHEM))
-        PlaySound.IS_EXECUTING = True
+        try:
+            voice_client: discord.VoiceClient = await PlaySound.PRISON_CHANNEL.connect(reconnect=False)
+            voice_client.play(discord.FFmpegPCMAudio(source=NATIONAL_ANTHEM))
+            PlaySound.IS_EXECUTING = True
+
+        except Exception as e:
+            PlaySound.IS_EXECUTING = False
+            raise e
 
         await asyncio.sleep(VC_STAY_LENGTH)
         await voice_client.disconnect(force=True)
-
-    # async def do(self):
-    #     """
-    #     実際にPartyIchiyoを実行する
-    #     """
-    #     voice_client = await self.base_voice_channel.connect(reconnect=False)
-    #     if self.music == 0:
-    #         chosen_music = random.choice(list(PartyIchiyo.MUSICS_LIST.values()))
-    #         voice_client.play(discord.FFmpegPCMAudio(chosen_music))
-    #     else:
-    #         chosen_music = PartyIchiyo.MUSICS_LIST[self.music]
-    #         voice_client.play(discord.FFmpegPCMAudio(chosen_music))
-    #     await self.kikisen_channel.send("パーティー Nigth")
-    #     sleep_time = MP3(chosen_music).info.length
-    #     await asyncio.sleep(sleep_time + 0.5)
-    #     await voice_client.disconnect(force=True)
